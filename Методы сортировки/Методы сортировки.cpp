4317357c7c1cbcf7f88062742948477ecf7f790c
Данил Калеев <KaleevDA@stud.kai.ru>
2022-05-23 03:15:25 +0300

    Методы сортировки

--- Методы сортировки/Методы сортировки.cpp
+++ Методы сортировки/Методы сортировки.cpp
@@ -1,7 +1,57 @@
 ﻿// Методы сортировки.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 #include <iostream>
 
-void Buble_sort(int* arr, int n, int& count_of_comparison, int& count_of_exchanges, int& overhead_costs)
+int getValue(int condition, const char* string)
+{
+	while (true)													// цикл продолжается до тех пор, пока пользователь не введет корректное значение
+	{
+		int a;
+		std::cin >> a;
+
+		if (std::cin.fail())										// если предыдущее извлечение оказалось неудачным,
+		{
+			std::cout << "Ошибка! Пожалуйста, повторите ввод! \n";
+			std::cin.clear();										// то возвращаем cin в 'обычный' режим работы
+			std::cin.ignore(32767, '\n');							// и удаляем значения предыдущего ввода из входного буфера
+			std::cout << string;
+		}
+		else // если всё хорошо и выполнятся условия, то возвращаем a
+		{
+			switch (condition)
+			{
+			case 15:
+				if ((a > 0)&&(a < 100000)) { return a; }
+
+				std::cout << "Вы должны ввести натуральное число, меньше 100000\n";
+				std::cin.ignore(32767, '\n');						// и удаляем значения предыдущего ввода из входного буфера
+				continue;
+
+			case 16:												// На будущее
+
+				if ((a == 0) || (a == 1)) { return a; }
+
+				std::cout << "Вы должны ввести либо 0, либо 1\n";
+				std::cin.ignore(32767, '\n');						// и удаляем значения предыдущего ввода из входного буфера
+				continue;
+			case 17:
+
+				if (((int)a > 0) && ((int)a < 6)) { return a; }
+
+				std::cout << "У меню всего 4 функции! Повторите ввод: ";
+				std::cin.ignore(32767, '\n');						// и удаляем значения предыдущего ввода из входного буфера
+				continue;
+
+			default:
+				return a;
+			}
+
+		}
+
+	}
+}
+
+
+void Buble_sort(int* arr, int n, unsigned long long& count_of_comparison, unsigned long long& count_of_exchanges, unsigned long long& overhead_costs)
 {
 	for (size_t i = 1; i < n; i++)
 	{
@@ -13,7 +63,7 @@ void Buble_sort(int* arr, int n, int& count_of_comparison, int& count_of_exchang
 				arr[j - 1] = arr[j];
 				arr[j] = temp;
 				count_of_exchanges++;
-				overhead_costs += 2;
+				//overhead_costs += 2;
 			}
 			count_of_comparison++;
 		}
@@ -21,13 +71,78 @@ void Buble_sort(int* arr, int n, int& count_of_comparison, int& count_of_exchang
 	}
 }
 
-void Insertion_sort(int* arr, int n, int& count_of_comparison, int& count_of_exchanges, int& overhead_costs)
+void Insertion_sort(int* arr, int n, unsigned long long& count_of_comparison, unsigned long long& count_of_exchanges, unsigned long long& overhead_costs)
+{
+	for (size_t i = 1; i < n; i++)
+	{
+		int j = i - 1; //overhead_costs++
+
+		while ((++count_of_comparison)&&(j >= 0)&&(arr[i] < arr[j]))
+		{
+			j--; // overhead_costs++
+		}
+
+		if ((j + 1) != i)
+		{
+			int temp = arr[i];	int l = i - 1;	//overhead_costs+=2;
+			while (l >= j + 1)
+			{
+				arr[l + 1] = arr[l]; l--;	//overhead_costs+=2;
+				count_of_exchanges++;
+			}
+			arr[j + 1] = temp;		//overhead_costs++;
+		}
+
+	}
+}
+
+void Selection_sort(int* arr, int n, unsigned long long& count_of_comparison, unsigned long long& count_of_exchanges, unsigned long long& overhead_costs)
+{
+	for (size_t i = 0; i < n - 1; i++)
+	{
+		int min = i; 
+		for (size_t j = i + 1; j < n; j++)
+		{
+			count_of_comparison++;
+			if (arr[j] < arr[min]) 
+			{ 
+				min = j; //overhead_costs++;
+			}
+		}
+		if (i != min)
+		{
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+			count_of_exchanges++;
+			//overhead_costs += 3;
+		}
+	}
+}
+
+/*
+void Selection_sort_Book(int* arr, int n, int& count_of_comparison, int& count_of_exchanges, int& overhead_costs)
+{
+	for (size_t i = 0; i < n-1; i++)
+	{
+		int min = i; int temp = arr[min];
+		for (size_t j= i+1; j < n; j++)
+		{
+			count_of_comparison++;
+			if (arr[j] < temp) { min = j, temp = arr[j]; count_of_exchanges++; overhead_costs++; }
+		}
+		arr[min] = arr[i]; arr[i] = temp;
+		overhead_costs += 4;
+
+	}
+}
+void Insertion_sort_Book(int* arr, int n, int& count_of_comparison, int& count_of_exchanges, int& overhead_costs)
 {
 	for (size_t i = 1; i < n; i++)
 	{
 		int temp = arr[i]; int j = i - 1;
 
-		while ((++count_of_comparison) && (j>=0) && (temp < arr[j]))
+		while ((++count_of_comparison) && (j >= 0) && (temp < arr[j]))
 		{
 			arr[j + 1] = arr[j]; j--;
 			count_of_exchanges++;
@@ -36,54 +151,119 @@ void Insertion_sort(int* arr, int n, int& count_of_comparison, int& count_of_exc
 		overhead_costs += 3;
 	}
 }
+*/
 
-void Selection_sort(int* arr, int n, int& count_of_comparison, int& count_of_exchanges, int& overhead_costs)
+int* Make_arr(int size)
 {
-	for (size_t i = 0; i < n-1; i++)
+	int* temp = new int[size];
+	for (size_t i = 0; i < size; i++)
 	{
-		int k = i; int temp = arr[i];
-		for (size_t j= i+1; j < n; j++)
-		{
-			count_of_comparison++;
-			if (arr[j] < temp) { k = j, temp = arr[j]; count_of_exchanges++; overhead_costs++; }
-		}
-		arr[k] = arr[i]; arr[i] = temp;
-		overhead_costs += 4;
+		temp[i] = rand() % 100000;
+	}
+	return temp;
+}
 
+void menu()
+{
+	std::cout << "\n\nФункционал программы:\n"
+		<< "1. Метод обмена\n"
+		<< "2. Метод Вставок\n"
+		<< "3. Метод Выбора\n"
+		<< "4. Вывести меню\n"
+		<< "5. Завершить работу\n";
+}
+
+int* Copy(int* arr, int size)
+{
+	int* temp = new int[size];
+	for (size_t i = 0; i < size; i++)
+	{
+		temp[i] = arr[i];
+	}
+	return temp;
+}
+void Show_arr(int* arr, int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		if (!(i % 15)) { std::cout << std::endl; }
+		std::cout << arr[i] << "\t";
 	}
 }
 
-
 int main()
 {
-	// { 15, 33, 42, 7, 12, 19 }
-	// { 88, 55, 44, 22, 11, 2}
-	// { 2, 4, 8, 9, 10, 16}
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int* arr = nullptr;
 
-	const int size = 6;
+	std::cout << "Введите размер массива: "; int size = getValue(15, "Введите размер массива: ");
+	
+	arr = Make_arr(size);
+	std::cout << "\nИсходный массив:\n"; Show_arr(arr, size);
 
-	int arr1[size] = { 88, 55, 44, 22, 11, 2 };
-	int count_of_comparison1 = 0;
-	int count_of_exchanges1 = 0;
-	int overhead_costs1 = 0;
+	int user_choice = -1;
+
+	menu(); std::cout << "\nВыберите действие:";
+	user_choice = getValue(17, "Выберите действие:");
+
+	while (user_choice != 5)
+	{
+		int* copy = nullptr;
+		int temp = 0;
+		unsigned long long count_of_comparison = 0;
+		unsigned long long count_of_exchanges = 0;
+		unsigned long long overhead_costs = 0;
+
+		if (user_choice != 4)
+		{
+			std::cout << "\nВыводить отсортированный массив? 0 - нет, 1 - да\nВведите: ";
+			temp = getValue(16, "Выводить отсортированный массив? 0 - нет, 1 - да\nВведите: ");
+
+			copy = Copy(arr, size);
+		}
 
 
-	int arr2[size] = { 88, 55, 44, 22, 11, 2 };
-	int count_of_comparison2 = 0;
-	int count_of_exchanges2 = 0;
-	int overhead_costs2 = 0;
+		switch (user_choice)
+		{
+		case 1:
+			Buble_sort(copy, size, count_of_comparison, count_of_exchanges, overhead_costs);
+			std::cout << "\n  Метод обмена ";
+			break;
+		case 2:
+			Insertion_sort(copy, size, count_of_comparison, count_of_exchanges, overhead_costs);
+			std::cout << "\n  Метод Вставок ";
+			break;
+		case 3:
+			Selection_sort(copy, size, count_of_comparison, count_of_exchanges, overhead_costs);
+			std::cout << "\n  Метод Выбора ";
+			break;
+		case 4:
+			menu();
+			break;
+		default:
+			break;
+		}
 
-	int arr3[size] = { 88, 55, 44, 22, 11, 2 };
-	int count_of_comparison3 = 0;
-	int count_of_exchanges3 = 0;
-	int overhead_costs3 = 0;
+		if (user_choice != 4)
+		{
+			std::cout << "Анализ:\n" << "1. Количество сравнений - " << count_of_comparison
+				<< "2. Количество перестановок - " << count_of_exchanges << std::endl;
+			//	<< "3. Накладные расходы - " << overhead_costs << std::endl << std::endl;
+		}
 
-	Buble_sort(arr1, size, count_of_comparison1, count_of_exchanges1, overhead_costs1);
-	Insertion_sort(arr2, size, count_of_comparison2, count_of_exchanges2, overhead_costs2);
-	Selection_sort(arr3, size, count_of_comparison3, count_of_exchanges3, overhead_costs3);
+		if (temp)
+		{
+			std::cout << "Исходный массив:\n"; Show_arr(arr, size);
+			std::cout << "\n\nОтсортированный массив:\n"; Show_arr(copy, size);
+		}
 
-	std::cout << "First Arr:  " << "count of comparison - " << count_of_comparison1 << "\tcount of exchanges - " << count_of_exchanges1 << "\toverhead costs - " << overhead_costs1 << std::endl;
-	std::cout << "Second Arr: " << "count of comparison - " << count_of_comparison2 << "\tcount of exchanges - " << count_of_exchanges2 << "\toverhead costs - " << overhead_costs2 << std::endl;
-	std::cout << "Third Arr:  " << "count of comparison - " << count_of_comparison3 << "\tcount of exchanges - " << count_of_exchanges3 << "\toverhead costs - " << overhead_costs3 << std::endl;
 
+		delete copy; copy = nullptr;
+
+	std::cout << "\nВыберите действие:";
+		user_choice = getValue(17, "Выберите действие:");
+	}
+
+	delete arr;
 }
